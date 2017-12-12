3eb29518b9c6249cd27d75cfc8792a34072bd311
HyunSeok Park <rhslvkf@gmail.com>
2017-12-12 23:53:57 +0900

    3.4.3.1 요구사항 1 - index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,18 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line = br.readLine();
+        	if (line == null) return;
+        	String[] tokens = line.split(" ");
+        	while (!line.equals("")) {
+        		log.debug(line);
+        		line = br.readLine();
+        	}
+        	String url = tokens[1];
+        	log.debug("url : {}", url);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "어서오세요!!!".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
