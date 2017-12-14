5973e1ddad4c629fa374125dbf4db5479c13bb4b
HyunSeok Park <rhslvkf@gmail.com>
2017-12-14 22:35:48 +0900

    4.2.1.2 요구사항 2 - GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -39,7 +43,17 @@ public class RequestHandler extends Thread {
         	String url = tokens[1];
         	log.debug("url : {}", url);
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            byte[] body = {};
+            if (url.startsWith("/user/create")) {
+            	int index = url.indexOf("?");
+            	String queryString = url.substring(index + 1);
+            	Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+            	User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+            	log.debug("User : {} ", user);
+            	body = Files.readAllBytes(new File("./webapp/index.html").toPath());
+            } else {
+            	body = Files.readAllBytes(new File("./webapp" + url).toPath());
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
